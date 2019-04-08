1ea7d8a6d407629fc38bdbab805363eef9bc25cc
luiizHF <luiiz0828@gamil.com>
2019-04-08 10:47:45 -0600

    sea crea estructura de backend

--- Backend/SistemaEventos-Server/Server/server.js
+++ Backend/SistemaEventos-Server/Server/server.js
@@ -1,8 +1,52 @@
-const http = require('http');
-const app = require('../app');
+require('../config/config');
+// import * as cors from 'cors';
 
-const port = process.env.PORT || 3000;
+const express = require('express');
+const mongoose = require('mongoose');
 
-const server = http.createServer(app);
 
-server.listen(port);
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// =====================
+// Verificar cors
+// =====================
+
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Acept, Athorization"
+//     );
+//     if (req.method === 'OPTIONS') {
+//         res,
+//         header('Access-Control-Allow-Mthods', 'GET; POST, PUT, DELETE, PATCH');
+//         return res.status(200).json({});
+//     }
+// })
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(require('../routes/user.route'));
+
+
+
+mongoose.connect(process.env.URLDB, { useNewUrlParse: true, useCreateIndex: true },
+    (err, res) => {
+
+        if (err) throw err;
+
+        console.log('Base de datos ONLINE');
+
+    }
+);
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
